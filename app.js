7cf2b79f40adf49553551280e7e6830b4f1bf4ce
cdsalty <soltiscd@gmail.com>
2020-07-16 15:51:37 -0400

    db connection established

--- app.js
+++ app.js
@@ -1,5 +1,10 @@
 const express = require("express");
+const connectDB = require("./config/db");
+
 const app = express();
+
+// Connect Database
+connectDB();
 
 // first endpoint:
 app.get("/", (req, res) =>
