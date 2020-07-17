28cfcdfa2a5ce8a4d65740f3402d52941b8426b8
cdsalty <soltiscd@gmail.com>
2020-07-16 23:56:57 -0400

    User model schema setup, working on route functionality

--- routes/users.js
+++ routes/users.js
@@ -2,11 +2,17 @@
 const express = require("express");
 const router = express.Router();
 
+const User = require("../models/User");
+
 // @route url:    POST api/users
 // @description:  Register a user
 // @access perm:  Public
 router.post("/", (req, res) => {
-	res.send("Register a user");
+	res.send(req.body);
 });
 
 module.exports = router;
+
+/*
+- to use req.body, must add middleware inside app.js
+*/
