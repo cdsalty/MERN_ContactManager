06d817920fe45e16848e725ca864c02a091239fa
cdsalty <soltiscd@gmail.com>
2020-07-20 23:51:38 -0400

    created contact model, get all contacts for user route

---
+++ models/Contact.js
@@ -0,0 +1,37 @@
+const mongoose = require("mongoose");
+
+const ContactSchema = mongoose.Schema({
+	// create relationship between contacts and users (each user has their own set of contacts)
+	user: {
+		type: mongoose.Schema.Types.ObjectId, // each document type, "model" have their own id;
+		refer: "users" // reference the specific collection in the object
+	},
+	// The basic setup/data-points you want to have in your contact list... THE SCHEMA
+	name: {
+		type: String,
+		required: true
+	},
+
+	email: {
+		type: String,
+		required: true
+		//unique: true  <-- only required in the user schema
+	},
+
+	phone: {
+		type: Number
+		// removed required but will change if necessary
+	},
+
+	type: {
+		type: String,
+		default: "personal" // personal or professional
+	},
+
+	date: {
+		type: Date,
+		default: Date.now()
+	}
+});
+
+module.exports = mongoose.model("contact", ContactSchema);
--- routes/contacts.js
+++ routes/contacts.js
@@ -1,34 +1,90 @@
-// will serve as primary crud route(GET, POST, PUT, DELETE)
+// To serve as primary crud route (GET, POST, PUT, DELETE)
+
 const express = require("express");
 const router = express.Router();
+const auth = require("../middleware/auth");
+const {check, validationResult} = require("express-validator"); // https://express-validator.github.io/docs/ (terminal states '/check' is deprecated)
 
-// @route url:    GET api/contacts
+const User = require("../models/User");
+const Contact = require("../models/Contact");
+
+// @route url:    GET api/contacts	(http://localhost:5000/api/contacts)
 // @description:  Get all user's contacts (not all contacts in database)
-// @access perm:  Private
-router.get("/", (req, res) => {
-	res.send("Get all contacts");
+// @access perm:  Private (will need to bring in auth middleware for protected routes like this)
+router.get("/", auth, async (req, res) => {
+	try {
+		const contacts = await Contact.find({user: req.user.id}).sort({date: -1}); // -1 will sort by most recent
+		// return the contacts from above:
+		res.json(contacts);
+	} catch (error) {
+		console.error(`error message from getting contacts is ${err.message}`);
+		res.status(500).send("Server Error");
+	}
 });
 
-// @route url:    POST api/contacts
-// @description:  Add a new contact
-// @access perm:  Private
-router.post("/", (req, res) => {
-	res.send("Add new contact");
-});
+// ------			------			------			------			------			------			------			------			------			------
+
+// @route url:    POST api/contacts (http://localhost:5000/api/contacts)
+// @description:  Add a New Contact
+// @access perm:  Private **
+router.post(
+	"/",
+	[
+		auth,
+		[
+			// check to make sure name is not left empty
+			check("name", "Name is required")
+				.not()
+				.isEmpty()
+		]
+	],
+	async (req, res) => {
+		// Start with error checking
+		const errors = validationResult(req); // brought in at the top; next check if it's empty
+		if (!errors.isEmpty()) {
+			return res.status(400).json({
+				errors: errors.array() // will return an array of all errors
+			});
+		}
+		// pull out the data in the body
+		const {name, email, phone, type} = req.body;
+
+		try {
+			// Create the data/layout for a New contact:
+			const newContact = new Contact({
+				name,
+				email,
+				phone,
+				type,
+				user: req.user.id
+			});
+			// create the contact and save
+			const contact = await newContact.save();
+
+			res.json(contact);
+		} catch (error) {}
+	}
+);
+
+// ------			------			------			------			------			------			------			------			------			------
 
 // @route url:    PUT api/contacts/:id (if you are going to update a contact, you need to be able to update a SPECIFIC(:id) contact)
 // @description:  Update contact
-// @access perm:  Private
+// @access perm:  Private **
 router.put("/:id", (req, res) => {
-	// always remember to update the route with ":id"
+	// remember to update route with ":id"
 	res.send("Update a contact");
 });
 
+// ------			------			------			------			------			------			------			------			------			------
+
 // @route url:    DELETE api/contacts/:id
 // @description:  Delete contact
-// @access perm:  Private
+// @access perm:  Private **
 router.delete("/:id", (req, res) => {
 	res.send("Delete a contact");
 });
+
+// ------			------			------			------			------			------			------			------			------			------
 
 module.exports = router;
