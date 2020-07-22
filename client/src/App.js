c25ab7446a19e2f5b8d78a82cf19672a72a278b4
cdsalty <soltiscd@gmail.com>
2020-07-22 00:40:46 -0400

    Setup Navbar and currently wrapping up the routing

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,28 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import Navbar from "./components/layout/Navbar";
+import Home from "./components/pages/Home";
+import About from "./components/pages/About";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+	return (
+		// Surround everything from the return around "Router" <Router>
+		<Router>
+			<React.Fragment>
+				<Navbar />
+				<div className='container'>
+					<Switch>
+						{/* Temp setup until I create the Home and About page */}
+						<Route exact path='/' component={Home} />
+						<Route exact path='/about' component={About} />
+					</Switch>
+				</div>
+			</React.Fragment>
+		</Router>
+	);
+};
 
 export default App;
+
+// Why use Fragment over React.Fragment or <></> ?
---
+++ client/src/components/pages/About.js
@@ -0,0 +1,17 @@
+import React from "react";
+
+const About = () => {
+	return (
+		<div>
+			<h2>About This App</h2>
+			<p className='my-2'>
+				This a MERN Application for keeping a list of professional and personal contacts
+			</p>
+			<p className='bg-dark p'>
+				<strong>Version: </strong> 1.0.0
+			</p>
+		</div>
+	);
+};
+
+export default About;
