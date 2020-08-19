38233ee99ae56db1499390bf7a256b9fc6ecc29a
cdsalty <soltiscd@gmail.com>
2020-08-19 01:00:14 -0400

    added required and min of 6 letters for password

--- client/src/components/auth/Register.js
+++ client/src/components/auth/Register.js
@@ -45,19 +45,33 @@ const Register = () => {
 			<form onSubmit={onSubmit}>
 				<div className='form-group'>
 					<label htmlFor='name'>Name</label>
-					<input type='text' name='name' value={name} onChange={onChange} />
+					<input type='text' name='name' value={name} onChange={onChange} required />
 				</div>
 				<div className='form-group'>
 					<label htmlFor='email'>Email</label>
-					<input type='email' name='email' value={email} onChange={onChange} />
+					<input type='email' name='email' value={email} onChange={onChange} required />
 				</div>
 				<div className='form-group'>
 					<label htmlFor='password'>Password</label>
-					<input type='password' name='password' value={password} onChange={onChange} />
+					<input
+						type='password'
+						name='password'
+						value={password}
+						onChange={onChange}
+						required
+						minLength='6'
+					/>
 				</div>
 				<div className='form-group'>
 					<label htmlFor='password2'>Confirm Password</label>
-					<input type='password' name='password2' value={password2} onChange={onChange} />
+					<input
+						type='password'
+						name='password2'
+						value={password2}
+						onChange={onChange}
+						required
+						minLength='6'
+					/>
 				</div>
 				<input type='submit' value='Register' className='btn btn-primary btn-block' />
 			</form>
